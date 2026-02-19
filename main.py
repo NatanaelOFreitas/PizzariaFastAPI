0561ad85e1796cbab1db7b2a746e4a47627e3430
Natanael Freitas <naelflashtw@gmail.com>
2026-02-19 15:03:55 -0400

    delete(surpresa): Remoção da feature de surpresa da API

--- main.py
+++ main.py
@@ -4,10 +4,8 @@ app = FastAPI()
 
 from auth_routes import auth_router
 from order_routes import order_router
-from surpresa_routes import surpresa_router
 
 app.include_router(auth_router)
 app.include_router(order_router)
-app.include_router(surpresa_router)
 
 #para rodar o código, basta rodar esse comando no terminal: uvicorn main:app --reload
--- surpresa_routes.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter
-
-surpresa_router = APIRouter(prefix="/surpresa", tags=["surpresa"])
-
-@surpresa_router.get("/")
-async def order():
-    return {"mensagem" : "Te amo papai"}
