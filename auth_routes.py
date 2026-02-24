a0249096117b64380bbdbfb06272945e595605a7
Natanael Freitas <naelflashtw@gmail.com>
2026-02-24 18:16:41 -0400

    fix(criar_token + criar_conta + bcrypt version): Correção das funções criar_token: a variavel de duração do token estava dentro da instância do datetime; e criar_usuario: o Usuario_schema estava sendo passado para dentro da função como uma variavel de mesmo nome fazendo que quando a variavel fosse chamado ele usava o schema e não a instância do schema; além da correção da versão do bcrypt que dava erro pela falta do ___about___

--- auth_routes.py
+++ auth_routes.py
@@ -10,18 +10,21 @@ from datetime import datetime, timedelta, timezone
 auth_router = APIRouter(prefix="/auth", tags=["auth"])
 
 def criar_token(id_usuario, duracao_token=timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)):
-    data_expiracao = datetime.now(timezone.utc + duracao_token)
+    data_expiracao = datetime.now(timezone.utc) + duracao_token
     dic_info = {"sub": id_usuario, "exp": data_expiracao}
     jwt_codificado = jwt.encode(dic_info, SECRET_KEY, ALGORITHM)
     return jwt_codificado
 
 def autenticar_usuario(email, senha, session):
-    usuario = Session.query(Usuario).filter(Usuario.email==email)
+    usuario = session.query(Usuario).filter(Usuario.email==email).first()
     if not usuario:
         return False
     elif not bcrypt_context.verify(senha, usuario.senha):
         return False
     return usuario
+
+#def verificar_token(token, session: Session = Depends(pegar_session)):
+    #usuario = 
 
 @auth_router.get("/")
 async def auth():
@@ -31,18 +34,18 @@ async def auth():
         }
 
 @auth_router.post("/criar_conta")
-async def criar_conta(Usario_schema: Usuario_schema, session: Session = Depends(pegar_session)):
-    usuario = session.query(Usuario).filter(Usuario.email==Usario_schema.email).first
+async def criar_conta(usuario_schema: Usuario_schema, session: Session = Depends(pegar_session)):
+    usuario = session.query(Usuario).filter(Usuario.email==usuario_schema.email).first()
     if usuario:
         #caso já exista um usuário
         raise HTTPException(status_code=400, detail= "Email já existente")
     else:
-        senha_criptografada = bcrypt_context.hash(Usuario_schema.senha)
-        novo_usuario = Usuario(Usario_schema.nome, Usario_schema.email, senha_criptografada, Usario_schema.ativo, Usario_schema.admin)
+        senha_criptografada = bcrypt_context.hash(usuario_schema.senha)
+        novo_usuario = Usuario(usuario_schema.nome, usuario_schema.email, senha_criptografada, usuario_schema.ativo, usuario_schema.admin)
         session.add(novo_usuario)
         session.commit()
         return {
-            "mensagem": f"Usuário cadastrado com sucesso, novo usuário: {Usario_schema.email}"
+            "mensagem": f"Usuário cadastrado com sucesso, novo usuário: {usuario_schema.email}"
             }
     
 @auth_router.post("/login")
@@ -51,14 +54,16 @@ async def login(login_schema: Login_schema, session: Session = Depends(pegar_ses
     if not usuario:
         raise HTTPException(status_code=400, detail="Usuário não encontrado ou senha errada")
     else:
-        acess_token = criar_token(usuario.id)
+        access_token = criar_token(usuario.id)
         refresh_token = criar_token(usuario.id, duracao_token=timedelta(days=7))
         return {
-                "acess_token": acess_token,
+                "acess_token": access_token,
                 "refresh_token": refresh_token,
                 "token_type": "Bearer"                
                 }
     
 @auth_router.get("/refresh")
-async def use_refresh_token():
+async def use_refresh_token(token):
+    access_token = criar_token()
+    #usuario = verificar_token()
     return 
